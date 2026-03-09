d72f8f8ab9d796eb498355aa52d119b220846faf
jo <git@akki.uk>
2026-03-09 17:21:26 +0000

    day02 p3

--- day/02/main.ts
+++ day/02/main.ts
@@ -156,6 +156,116 @@ function part2(data: string, logger: Logger) {
 }
 
 function part3(data: string, logger: Logger) {
+  const grid = new Grid(data.split('\n').map((line) => line.split('')), CoordSystem.Xy);
+
+  function find(char: string): Point2DLike {
+    const result = grid.find((value) => value === char);
+    if (!result) throw new Error(`could not find ${char}`);
+    return Utils.pick(result, ['x', 'y']);
+  }
+
+  const start = find('@');
+  const bones: Point2DLike[] = grid.findAll((value) => value === '#').map((item) => Utils.pick(item, ['x', 'y']))
+    .toArray();
+  const offsets = Point2D.offsets(1, Offset2D.Cardinal).map(({ x, y }) => ({ x, y: y * -1 }));
+  let current = { ...start };
+  let moves = 0;
+  let blocked = 0;
+  const occupied = new PackedMap(Point2D.pack32, Point2D.unpack32, [
+    [start, 'S'],
+    ...bones.map((bone) => [bone, 'B'] satisfies [Point2DLike, string]),
+  ]);
+  const bounds = {
+    minX: Math.min(start.x, ...bones.map(({ x }) => x)),
+    maxX: Math.max(start.x, ...bones.map(({ x }) => x)),
+    minY: Math.min(start.y, ...bones.map(({ y }) => y)),
+    maxY: Math.max(start.y, ...bones.map(({ y }) => y)),
+  };
+
+  function dump(current: Point2DLike): void {
+    // remap into +ve coordinate space
+    const offset = { x: -bounds.minX, y: -bounds.minY };
+    const debugGrid = new Grid(
+      { rows: bounds.maxY - bounds.minY + 1, cols: bounds.maxX - bounds.minX + 1, fill: '.' },
+      CoordSystem.Xy,
+    );
+    for (const [point, value] of occupied.entries()) debugGrid.cellSet(Point2D.add(point, offset), value);
+    debugGrid.cellSet(Point2D.add(start, offset), 'S');
+    for (const bone of bones) debugGrid.cellSet(Point2D.add(bone, offset), 'B');
+    debugGrid.cellSet(Point2D.add(current, offset), '@');
+    logger.debugLow(debugGrid);
+  }
+
+  function floodFill(origin: Point2DLike): void {
+    if (occupied.has(origin)) return;
+    // map contiguous unoccupied region
+    // abort if it includes bounds
+    // set occupied
+    const queue = new Deque<Point2DLike>();
+    queue.pushFront(origin);
+    const region = new PackedSet(Point2D.pack32, Point2D.unpack32);
+    let abortFlag = false;
+
+    while (!abortFlag) {
+      const item = queue.popBack();
+      if (!item) break;
+      if (occupied.has(item) || region.has(item)) continue;
+      if (item.x === bounds.minX || item.x === bounds.maxX || item.y === bounds.minY || item.y === bounds.maxY) {
+        abortFlag = true;
+        break;
+      }
+      region.add(item);
+      for (const neighbour of offsets.map((offset) => Point2D.add(offset, item))) queue.pushFront(neighbour);
+    }
+
+    if (abortFlag || region.size === 0) return;
+    for (const point of region) occupied.set(point, 'F');
+    logger.debugLow('flood fill', ...region.keys().toArray());
+  }
+
+  function isEndEnclosed(): boolean {
+    return bones.every((bone) =>
+      occupied.has({ x: bone.x - 1, y: bone.y }) &&
+      occupied.has({ x: bone.x + 1, y: bone.y }) &&
+      occupied.has({ x: bone.x, y: bone.y - 1 }) &&
+      occupied.has({ x: bone.x, y: bone.y + 1 })
+    );
+  }
+
+  // pre-fill voids between bones or `isEndEnclosed` will never return true
+  for (let x = bounds.minX; x <= bounds.maxX; ++x) {
+    for (let y = bounds.minY; y <= bounds.maxY; ++y)
+      floodFill({ x, y });
+  }
+
+  for (let step = 0; !isEndEnclosed(); ++step) {
+    if (blocked > 12) {
+      dump(current);
+      throw new Error('blocked');
+    }
+    const offset = offsets[Math.floor(step / 3) % offsets.length];
+    const next = Point2D.add(current, offset);
+    logger.debugLow({ step, moves, current, offset, next });
+    if (occupied.has(next)) {
+      ++blocked;
+      continue;
+    }
+    occupied.set(next, '+');
+    current = next;
+    ++moves;
+    blocked = 0;
+    bounds.minX = Math.min(bounds.minX, current.x);
+    bounds.maxX = Math.max(bounds.maxX, current.x);
+    bounds.minY = Math.min(bounds.minY, current.y);
+    bounds.maxY = Math.max(bounds.maxY, current.y);
+    for (const neighbour of offsets.map((offset) => Point2D.add(offset, current))) floodFill(neighbour);
+    dump(current);
+  }
+
+  dump(current);
+
+  // 2486
+  logger.success(moves);
 }
 
 function main() {
