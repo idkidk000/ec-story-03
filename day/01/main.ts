be5d91e7352983315b42c17b5b271903007944b9
jo <git@akki.uk>
2026-03-09 13:12:23 +0000

    day01 tidy up

--- day/01/main.ts
+++ day/01/main.ts
@@ -1,24 +1,17 @@
 import { EcArgParser } from '@/lib/args.1.ts';
 import { Logger } from '@/lib/logger.0.ts';
 
-function parseComponent(data: string, logger: Logger): number {
-  const values = data.split('').map((t, i) => t.charCodeAt(0) < 97 ? (2 << (4 - i)) || 1 : 0);
-  const result = values.reduce((acc, item) => acc + item, 0);
-  logger.debugMed({ data, values, result });
-  return result;
+interface Parsed {
+  id: number;
+  red: number;
+  green: number;
+  blue: number;
+  value: number;
+  shine: number;
 }
 
-function part1(data: string, logger: Logger) {
-  const parsed = data.split('\n').map((line) => {
-    const tokens = line.split(/[: ]/);
-    return {
-      id: parseInt(tokens[0]),
-      red: parseComponent(tokens[1], logger),
-      green: parseComponent(tokens[2], logger),
-      blue: parseComponent(tokens[3], logger),
-    };
-  });
-  const result = parsed.filter((item) => item.green > item.blue && item.green > item.red).reduce(
+function part1(data: Parsed[], logger: Logger) {
+  const result = data.filter((item) => item.green > item.blue && item.green > item.red).reduce(
     (acc, item) => acc + item.id,
     0,
   );
@@ -27,50 +20,23 @@ function part1(data: string, logger: Logger) {
   logger.success(result);
 }
 
-function part2(data: string, logger: Logger) {
-  const parsed = data.split('\n').map((line) => {
-    const tokens = line.split(/[: ]/);
-    return {
-      id: parseInt(tokens[0]),
-      red: parseComponent(tokens[1], logger),
-      green: parseComponent(tokens[2], logger),
-      blue: parseComponent(tokens[3], logger),
-      shine: parseComponent(tokens[4], logger),
-    };
-  });
-  logger.debugLow(parsed);
-  const maxShine = parsed.toSorted((a, b) => b.shine - a.shine)[0].shine;
-  logger.debugLow({ maxShine });
-  const result = parsed.filter((item) => item.shine === maxShine).map((item) => ({
-    ...item,
-    value: item.blue + item.green + item.red,
-  })).toSorted((a, b) => a.value - b.value)[0].id;
+function part2(data: Parsed[], logger: Logger) {
+  const maxShine = data.toSorted((a, b) => b.shine - a.shine)[0].shine;
+  const result = data.filter((item) => item.shine === maxShine).toSorted((a, b) => a.value - b.value)[0].id;
 
   // 75681
   logger.success(result);
 }
 
-function part3(data: string, logger: Logger) {
-  const parsed = data.split('\n').map((line) => {
-    const tokens = line.split(/[: ]/);
-    return {
-      id: parseInt(tokens[0]),
-      red: parseComponent(tokens[1], logger),
-      green: parseComponent(tokens[2], logger),
-      blue: parseComponent(tokens[3], logger),
-      shine: parseComponent(tokens[4], logger),
-    };
-  }).map((item) => ({ ...item, value: item.blue + item.green + item.red }));
-  logger.debugLow(parsed);
-
+function part3(data: Parsed[], logger: Logger) {
   const groups = Array.from({ length: 6 }, () => new Array<number>());
 
-  for (const item of parsed) {
-    const dominant = item.blue > item.green && item.blue > item.red
+  for (const item of data) {
+    const dominant = item.red > item.blue && item.red > item.green
       ? 0
       : item.green > item.blue && item.green > item.red
       ? 1
-      : item.red > item.blue && item.red > item.green
+      : item.blue > item.green && item.blue > item.red
       ? 2
       : null;
     const shine = item.shine <= 30 ? 0 : item.shine >= 33 ? 1 : null;
@@ -79,8 +45,7 @@ function part3(data: string, logger: Logger) {
   }
   logger.debugLow(groups);
 
-  const largest = groups.toSorted((a, b) => b.length - a.length)[0];
-  const result = largest.reduce((acc, item) => acc + item, 0);
+  const result = groups.toSorted((a, b) => b.length - a.length)[0].reduce((acc, item) => acc + item, 0);
 
   // 10492858
   logger.success(result);
@@ -88,9 +53,30 @@ function part3(data: string, logger: Logger) {
 
 function main() {
   const { data, logger, part } = new EcArgParser(import.meta.url);
-  if (part === 1) part1(data, logger.makeChild('part1'));
-  if (part === 2) part2(data, logger.makeChild('part2'));
-  if (part === 3) part3(data, logger.makeChild('part3'));
+
+  function parseComponent(component: string): number {
+    return component.split('').map((t, i) => t.charCodeAt(0) < 97 ? (2 << (4 - i)) || 1 : 0).reduce(
+      (acc, item) => acc + item,
+      0,
+    );
+  }
+
+  const parsed = data.split('\n').map((line) => {
+    const tokens = line.split(/[: ]/);
+    const [red, green, blue] = tokens.slice(1, 4).map(parseComponent);
+    return {
+      id: parseInt(tokens[0]),
+      red,
+      green,
+      blue,
+      value: red + green + blue,
+      shine: tokens.length > 4 ? parseComponent(tokens[4]) : 0,
+    };
+  });
+
+  if (part === 1) part1(parsed, logger.makeChild('part1'));
+  if (part === 2) part2(parsed, logger.makeChild('part2'));
+  if (part === 3) part3(parsed, logger.makeChild('part3'));
 }
 
 main();
